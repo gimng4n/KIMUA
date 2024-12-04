1dcbd0f8dbbfec623a3aa6674b96d52074d11be4
gimng4n <gimng4n@gmail.com>
2024-12-04 16:20:32 -0600

    Update KIMIA_project2.ino
    - Added comments
    - Organize code
    - Changed inputs for some servos (playtesting)

--- KIMIA_project2.ino
+++ KIMIA_project2.ino
@@ -1,12 +1,21 @@
+/*
+Once Upon An Arduino - Indiana Jones
+TEAM: KIMIA
+MEMBERS: Kim Ngan Nguyen & Tamia Hoyt
+AET 313
+*/
+
+//  Assign pin number to buttons
 const int buttonOne = 10;
 const int buttonTwo = 8;
 const int buttonThree = 6;
 const int buttonFour = 5;
 
-
+//  Assign pin number to LEDs
 const int redPin = 12;
 const int redPin2 = 13;
 
+//  Static State Change Detections
 int buttonState1 = 0;
 int buttonState2 = 0;
 int buttonState3 = 0;
@@ -16,6 +25,7 @@ int previousButtonState2 = 0;
 int previousButtonState3 = 0;
 int previousButtonState4 = 0;
 
+//  Adding in all four servos
 #include <Servo.h>
 Servo servoOne;
 Servo servoTwo;
@@ -23,20 +33,25 @@ Servo servoThree;
 Servo servoFour;
 
 void setup() {
+  //  Assign pin number to each servo
   servoOne.attach(11);
   servoTwo.attach(9);
   servoThree.attach(7);
-  servoFour.attach(2);
+  servoFour.attach(4);
 
-  servoOne.write(110);  // 10 90
-  servoTwo.write(56);   // go down
-  servoThree.write(50); // 30
-  servoFour.write(180);
+  // reset all servos
+  servoOne.write(140); 
+  servoTwo.write(150);  
+  servoThree.write(10);
+  servoFour.write(90);
 
+  // set all buttons as INPUTs
   pinMode(buttonOne, INPUT);
   pinMode(buttonTwo, INPUT);
+  pinMode(buttonThree, INPUT);
+  pinMode(buttonFour, INPUT);
 
-  // LEDs setup + restart
+  // set LEDs as OUTPUTS and reset all LEDs
   pinMode(redPin, OUTPUT);
   pinMode(redPin2, OUTPUT);
   digitalWrite(redPin2, LOW);
@@ -46,17 +61,20 @@ void setup() {
 }
 
 void loop() {
+
+  //  Store current state of each button
   buttonState1 = digitalRead(buttonOne);
   buttonState2 = digitalRead(buttonTwo);
   buttonState3 = digitalRead(buttonThree);
   buttonState4 = digitalRead(buttonFour);
 
-  //  First Interaction (1 Servo + 2 LED)
+  //  First Interaction (1 Servo + 2 LEDs)
   if (buttonState1 != previousButtonState) {
     if (buttonState1 == HIGH) {
+      delay(2000);
       digitalWrite(redPin, HIGH);
       digitalWrite(redPin2, HIGH);
-      servoOne.write(33);
+      servoOne.write(43);
       Serial.println("button pressed");
     } else {
       Serial.println("button released");
@@ -67,7 +85,7 @@ void loop() {
   // Third Interaction (1 Servo Bridge)
   if (buttonState2 != previousButtonState2) {
     if (buttonState2 == HIGH) {
-      servoTwo.write(0);
+      servoTwo.write(100);
       Serial.println("button pressed");
     } else {
       Serial.println("button released");
@@ -75,14 +93,14 @@ void loop() {
   }
   previousButtonState2 = buttonState2;
 
-
-  // Third Interaction (1 Servo Axe)
+  // Fourth Interaction (1 Servo Axe)
   if (buttonState3 != previousButtonState3) {
     if (buttonState3 == HIGH) {
+      delay(2000);
       for (int i = 0; i <= 3; i++) { //action
-        servoThree.write(180);
-        delay(500);
         servoThree.write(110);
+        delay(500);
+        servoThree.write(10);
         delay(500);
       }
       Serial.println("button pressed");
@@ -95,7 +113,7 @@ void loop() {
     // Fifth Interaction (1 Servo Boulder)
   if (buttonState4 != previousButtonState4) {
     if (buttonState4 == HIGH) {
-      servoFour.write(90);
+      servoFour.write(0);
       Serial.println("button pressed");
     } else {
       Serial.println("button released");
